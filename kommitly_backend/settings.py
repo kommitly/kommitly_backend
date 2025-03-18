e0da62cc63f9b74265d1cfa1459b785313b908c6
Shanon Wambui <shanonwambui24@gmail.com>
2025-03-18 04:25:05 +0300

    Add channel redis

--- kommitly_backend/settings.py
+++ kommitly_backend/settings.py
@@ -252,3 +252,12 @@ CACHES = {
         }
     }
 }
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
